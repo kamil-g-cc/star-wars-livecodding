da5a4b1e016c0f2ffa2243ae1ad659b18a65a41e
R3qUi3M86 <requiem123@gmail.com>
2022-01-22 14:32:56 +0100

    addPlanet() added

---
+++ main.js
@@ -0,0 +1,35 @@
+let request = new XMLHttpRequest();
+let responseJSON = {results: null};
+let planetList = null
+
+function createAndSendRequest(){
+    request.onreadystatechange = onReadyStateChangeListener;
+    request.open("GET", "https://swapi.py4e.com/api/planets/", true);
+    request.send();
+}
+
+function onReadyStateChangeListener(){
+    console.log(request.readyState);
+    if (request.readyState === XMLHttpRequest.DONE && request.status === 200){
+        responseJSON = JSON.parse(request.response);
+        planetList = responseJSON.results;
+
+        //addPlanet()
+    }
+}
+
+function addPlanet(name, diameter){
+    let tableRow = document.createElement('tr');
+    let tableCell1 = document.createElement('td');
+    let tableCell2 = document.createElement('td');
+
+    tableCell1.innerText = name;
+    tableCell2.innerText = diameter;
+
+    tableRow.appendChild(tableCell1);
+    tableRow.appendChild(tableCell2);
+
+    document.getElementById('planets').appendChild(tableRow);
+}
+
+createAndSendRequest();
